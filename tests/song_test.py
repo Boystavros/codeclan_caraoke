68f271ba5c6c7fcf0bd6d46b4373faae01083e8e
Sacha Ponniah <sponniah812@gmail.com>
2021-11-19 11:45:28 +0000

    Creates Song class, tests and all tests passed

--- tests/song_test.py
+++ tests/song_test.py
@@ -1,2 +1,16 @@
-class TestSong():
-    pass
+import unittest
+from src.song import Song
+
+class TestSong(unittest.TestCase):
+    
+    def setUp(self):
+        self.song = Song("Dreams", "Fleetwood Mac", 257)
+
+    def test_has_name(self):
+        self.assertEqual("Dreams", self.song.name)
+
+    def test_has_artist(self):
+        self.assertEqual("Fleetwood Mac", self.song.artist)
+
+    def test_has_length(self):
+        self.assertEqual(257, self.song.length)
